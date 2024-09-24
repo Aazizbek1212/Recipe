292da173a4492ec3cb32efad0b81b78ec3f4234c
Aazizbek1212 <167636979+Aazizbek1212@users.noreply.github.com>
2024-09-24 21:08:32 +0500

    ''

--- Project/settings.py
+++ Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+qzxsg-*e-c5k!417jp)@c5^@3qa55cjd_y2jk5^53#93wox78
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
