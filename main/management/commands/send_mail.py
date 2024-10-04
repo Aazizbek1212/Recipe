bed38e3537a1bee3978c7940a644bbe37a1236fb
Aazizbek1212 <167636979+Aazizbek1212@users.noreply.github.com>
2024-10-04 17:29:48 +0500

    '7'

--- main/management/commands/send_mail.py
+++ main/management/commands/send_mail.py
@@ -7,6 +7,6 @@ class Command(BaseCommand):
     help = "Email jo'natish komandasi"
 
     def handle(self, *args, **kwargs):
-        send_mail('test mail', 'test email', 'azizbekilyosov1212@gmail.com',
+        send_mail('test mail', 'Vazifa bajarildi!', 'azizbekilyosov1212@gmail.com',
                   ['azizbekilyosov1212@gmail.com'], fail_silently=False)
         self.stdout.write(self.style.SUCCESS("Email muvaffaqiyatli jo'natildi!"))
